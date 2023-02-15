6fa56a55c50d38a49bef4aa3e12bc8c5b0e33a72
lucasnkz11 <lucasnkz11@gmail.com>
2023-02-15 01:43:55 -0300

    first schema added

---
+++ src/routes/dish/dish.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDishDto } from './dto/create-dish.dto';
+import { UpdateDishDto } from './dto/update-dish.dto';
+
+@Injectable()
+export class DishService {
+  create(createDishDto: CreateDishDto) {
+    return 'This action adds a new dish';
+  }
+
+  findAll() {
+    return `This action returns all dish`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} dish`;
+  }
+
+  update(id: number, updateDishDto: UpdateDishDto) {
+    return `This action updates a #${id} dish`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} dish`;
+  }
+}
